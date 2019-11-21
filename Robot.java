d7e30c6df65688b2e9da7872cb55d18547829c98
Bottle-o-Mercury <56655648+Bottle-o-Mercury@users.noreply.github.com>
2019-11-20 22:13:37 -0500

    ok

--- Robot.java
+++ Robot.java
@@ -1,9 +1,3 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
 package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
@@ -12,10 +6,7 @@ import frc.robot.commands.LiftCommand;
 import frc.robot.subsystems.*;
 import edu.wpi.first.wpilibj.command.*;
 
-/**
- * This is a demo program showing the use of the RobotDrive class, specifically
- * it contains the code necessary to operate a robot with tank drive.
- */
+
 public class Robot extends TimedRobot {
 
 
